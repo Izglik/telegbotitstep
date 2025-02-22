08220104b8de424948b960304d7865aa60fb5387
Қарабай Ізгілік <@step.kz>
2025-02-22 09:30:18 +0500

    Fourth commit

--- bot.py
+++ bot.py
@@ -1,27 +1,41 @@
 import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import (
+    ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+)
 from aiogram.filters import Command
-from api import TOKEN #из файла api добавляем tokenv
+from aiogram.client.default import DefaultBotProperties
+from api import TOKEN
 
-bot = Bot(token=TOKEN)
+# Создаем объект - бот и диспетчер
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
 dp = Dispatcher()
 
+# Основная клавиатура
+main_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Привет!"), KeyboardButton(text="Помощь")]
+    ],
+    resize_keyboard=True
+)
+
+# Инлайн клавиатура
+inline_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Перейти на сайт", url="http://example.com")],
+        [InlineKeyboardButton(text="Нажми", callback_data="button_click")]
+    ]
+)
+
 @dp.message(Command("start"))
-async def send_welcome(message: Message):
-    await message.answer("Привет! Я твой Telegram-бот 🤖")
+async def start(message: types.Message):
+    await message.answer("Привет! Я тестовый бот <b>test</b>", reply_markup=main_keyboard)
 
-@dp.message(Command("help"))
-async def send_welcome(message: Message):
-    await message.answer("Я ничего пока не умею, только команды /start а так же /help")
-
-@dp.message(Command("qwe"))
-async def send_welcome(message: Message):
-    await message.answer("rty")
+@dp.message(lambda message: message.text == "Привет!")
+async def hello(message: types.Message):
+    await message.answer("Привет!!! Как дела?", reply_markup=inline_keyboard)
 
 async def main():
-    print("Бот запущен")
-    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":

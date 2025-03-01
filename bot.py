745c2eb79b0b5e64c61c8e4d352d1b24ddb2a455
Қарабай Ізгілік <@step.kz>
2025-03-01 09:14:14 +0500

    eightth commit

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
+# Все нужные библиотеки
 import asyncio
 import random
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import (
-    ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-)
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton)
 from aiogram.filters import Command
 from aiogram.client.default import DefaultBotProperties
 from api import TOKEN
@@ -20,7 +19,7 @@ main_keyboard = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
-# Инлайн клавиатура
+# Инлайн клавиатура/Добовляет кнопки и какую команду оно выполняет
 inline_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text="Начать", callback_data="start")],
@@ -29,6 +28,7 @@ inline_keyboard = InlineKeyboardMarkup(
     ]
 )
 
+# Что выводит когда мы нажимаем 
 @dp.callback_query()
 async def callback_handler(callback: types.CallbackQuery):
     if callback.data == "start":
@@ -38,19 +38,23 @@ async def callback_handler(callback: types.CallbackQuery):
     elif callback.data == "random":
         await callback.message.answer("Хочешь рандомное число? Напиши /random")
 
+# Приветствует нас
 @dp.message(Command("start"))
 async def start(message: types.Message):
     await message.answer("Привет! Я тестовый бот <b>test</b>", reply_markup=main_keyboard)
 
+# Просто здорвается
 @dp.message(lambda message: message.text == "Привет!")
 async def hello(message: types.Message):
     await message.answer("Привет!!! Как дела?", reply_markup=inline_keyboard)
 
+# На комадну рандом выводит радномое число используя библиотеку random
 @dp.message(Command("random"))
 async def random_command(message: types.Message):
     number = random.randint(1, 100)
     await message.answer(f"Случайное число: {number}")
 
+# На команду помошь даем доступные на этот момент команды
 @dp.message(Command("help"))
 async def help_command(message: types.Message):
     command_text = (
@@ -60,8 +64,10 @@ async def help_command(message: types.Message):
         "/random - Генерирует рандомное число"
     )
 
+# Запускает бота
 async def main():
     await dp.start_polling(bot)
 
+# Для выполнение всех команд одноврененно и всегда(наверное)
 if __name__ == "__main__":
     asyncio.run(main())
